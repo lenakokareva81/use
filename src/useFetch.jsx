19a24b9b0ed3cebc6a4c24ba3a770ab301777cca
lenakokareva81 <lenakokareva81@gmail.com>
2026-02-12 17:42:24 +0300

    useFetch_refetch

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -2,34 +2,35 @@ import { useEffect, useRef, useState } from "react"
 
 export function useFetch(request) {
     const [data, setData] = useState([])
+    const [error, setError] = useState(false)
+    const [restart, setRestart] = useState(true)
+    const page = useRef(1)
+    const limit = useRef(5)
     const loading = useRef(true)
     const isLoading = loading.current
-    const [error, setError] = useState(false)
+
 
     useEffect(() => {
-        fetch(request)
+        fetch(request + `?_page= ${page.current}&_limit=${limit.current}`)
             .then(response => response.json())
             .then(json => {
-                console.log("все хорошо", json)
                 loading.current = false
                 setData(json)
             })
             .catch(error => {
-                console.log("ошибка", error)
-
                 loading.current = false
                 setError(true)
             })
-    }, [])
+    }, [restart])
 
-
-
-
-
-
-
-    function refetch() {
-        console.log("refetch")
+    function refetch({ params }) {
+        if (params._limit) {
+            limit.current = params._limit
+        }
+        console.log('params', params._limit)
+        page.current = page.current + 1
+        loading.current = true
+        setRestart(!restart)
     }
 
     return {
